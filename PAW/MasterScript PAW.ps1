1fbc254266696daddf9e446f8114aec1482b1d1d
Guillermo Diaz <guillermo.diaz@mandiant.com>
2021-10-27 13:38:07 +1100

    format script

--- PAW/MasterScript PAW.ps1
+++ PAW/MasterScript PAW.ps1
@@ -1,9 +1,7 @@
 <#
-
 .COPYRIGHT
 Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 See LICENSE in the project root for license information.
-
 #>
 
 # Determine script location for PowerShell
@@ -22,18 +20,14 @@ Function Set-AADAuth {
     NAME: Set-AADAuth
     #>
     
-    [cmdletbinding()]
-    
-    param
-    (
+    [cmdletbinding()]    
+    param(
         #[Parameter(Mandatory=$true)]
         $User
     )
     
-    Write-Host "Checking for AzureAD module..."
-    
-        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
-    
+    Write-Host "Checking for AzureAD module..."    
+        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable    
         if ($AadModule -eq $null) {
             write-host
             write-host "AzureAD Powershell module not installed..." -f Red
@@ -43,84 +37,54 @@ Function Set-AADAuth {
             #write-host "Script can't continue..." -f Red
             write-host
             #exit
-        }
+        }    
+        Connect-AzureAD -AccountId $user | Out-Null    
+}
     
-        Connect-AzureAD -AccountId $user | Out-Null
-    
-    }
-    
-####################################################
-    
+####################################################    
     $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
-
-    Set-AADAuth -user $user
+    Set-AADAuth -user $user    
+####################################################    
     
- ####################################################
-    
-    
- #write-host "Adding App Registrtion"
-
- #. $ScriptDir/AppRegistration_Create.ps1
- 
- #Start-Sleep -s 5
+#write-host "Adding App Registrtion"
+#. $ScriptDir/AppRegistration_Create.ps1
+#Start-Sleep -s 5
 
 #write-host "Adding required AAD Groups"
-
 # . $ScriptDir/AADGroups_Create.ps1
 
 #write-host "Adding AAD Group Membership"
-
 # . $ScriptDir/AADGroupMemberships_Add.ps1
-
 # Start-Sleep -s 5
 
 #write-host "Adding Named Locations"
-
 #. $ScriptDir/NamedLocations_Import.ps1 -user $user
-
 #Start-Sleep -s 5
 
 #write-host "Adding Conditional Access Policies"
-
 #. $ScriptDir/CA-Policies-Import_PAW.ps1 -State "Disabled"
-
 #Start-Sleep -s 5
 
 write-host "Adding Device Configuration Profiles"
-
 . $ScriptDir/Import-PAW-DeviceConfiguration.ps1
-
-#Start-Sleep -s 5
+Start-Sleep -s 5
 
 write-host "Adding Device Compliance Policies"
-
 . $ScriptDir/Import-PAW-DeviceCompliancePolicies.ps1
-
 Start-Sleep -s 5
 
 write-host "Adding Update Rings Policy"
-
 . $ScriptDir/Import-PAW-DeviceConfigurationADMX.ps1
-
 Start-Sleep -s 5
 
 #write-host "Adding Enrollment Status Page"
-
 #. $ScriptDir/ESP_Import.ps1
-
 #Start-Sleep -s 5
 
 #write-host "Adding AutoPilot Profile"
-
 #. $ScriptDir/AutoPilot_Import.ps1
-
 #Start-Sleep -s 5
 
 #write-host "Adding Device Enrollment Restrictions"
-
 #. $ScriptDir/DER-Import_PAW.ps1
-
 #Start-Sleep -s 5
-
-
-
